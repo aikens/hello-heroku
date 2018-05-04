02f933599546729153ffc07e60d782fd1a51f58f
Andrew Aikens <andrew.aikens@gmail.com>
2018-05-04 15:47:51 -0300

    bigger better Hello

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Context-Type', 'text/plain');
-	res.end('Hello World\n');
+	res.end('<html><h1>Hello World 2.0!</h1></html>\n');
 });
 
 server.listen(PORT, () => {
