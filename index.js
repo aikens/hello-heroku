d31c12335071357a5c1e32fb40a6c6fc588ddbd3
Andrew Aikens <andrew.aikens@gmail.com>
2018-05-04 16:18:19 -0300

    add tests

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const http = require('http');
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Context-Type', 'text/plain');
-	res.end('<html><h1>Hello World 2.0!</h1></html>\n');
+  res.statusCode = 200;
+  res.setHeader('Context-Type', 'text/plain');
+  res.end('<html><h1>Hello World 2.0!</h1></html>\n');
 });
 
 server.listen(PORT, () => {
-	console.log(`Server running on ${PORT}/`);
+  // eslint-disable-next-line
+  console.log(`Server running on ${PORT}/`);
 });
